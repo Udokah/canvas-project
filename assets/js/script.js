9fbf54b7a1a8de837cb855e20900948c3373cdb4
UD <udswagz@gmail.com>
2015-02-19 10:46:36 +0100

    added layering functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-var canvas = document.getElementById('canvas');
-var canvasClass = new CanvasClass(canvas);
-canvasClass.initialize();
-canvasClass.bindSettingsChangeListenters();
-canvasClass.bindDrawingModesChangeListener();
-
+var canvas1 = document.getElementById('canvas1');
+var canvas2 = document.getElementById('canvas2');
+var canvasClass ;
 var dragging = false ;
 
 var drawLine = function(e){
@@ -45,10 +42,51 @@ var disengage = function(){
     canvasClass.context.beginPath();
 };
 
+var initializeCanvas = function(canvas){
+    canvasClass = new CanvasClass(canvas);
+    canvasClass.initialize();
+    canvasClass.bindSettingsChangeListenters();
+    canvasClass.bindDrawingModesChangeListener();
+
+    /* Bind them listeners to the canvas */
+    canvas.addEventListener('mousedown', engage);
+    canvas.addEventListener('mouseup', disengage);
+    canvas.addEventListener('mousemove', drawLine);
+    canvas.addEventListener('click', drawShape);
+};
+
+initializeCanvas(canvas1);
+
 /*overlay effizy*/
 document.getElementById('overlay').addEventListener('dblclick', function(){
     this.style.display = "none";
 });
+
+/* Activate button for first layer */
+var layer1 = document.getElementById('layer1');
+var layer2 = document.getElementById('layer2');
+
+layer1.setAttribute("class","selected");
+
+layer1.addEventListener('click', function(){
+    layer2.removeAttribute("class");
+    this.setAttribute("class","selected");
+    canvas1.style.zIndex = 100 ;
+    canvas2.style.zIndex = 50 ;
+    initializeCanvas(canvas1);
+});
+
+layer2.addEventListener('click', function(){
+    layer1.removeAttribute("class");
+    this.setAttribute("class","selected");
+    canvas1.style.zIndex = 50 ;
+    canvas2.style.zIndex = 100 ;
+    initializeCanvas(canvas2);
+});
+
+
+
+
 
 /* button pick effect */
 /*
@@ -66,8 +104,3 @@ for(var i=0; i<toolBtn.length; i++){
 }
 */
 
-/* Bind them listeners to the canvas */
-canvas.addEventListener('mousedown', engage);
-canvas.addEventListener('mouseup', disengage);
-canvas.addEventListener('mousemove', drawLine);
-canvas.addEventListener('click', drawShape);
